976bf84a8ee7a73e631abe128bfc5e9654fee879
senthil121 <senthilrock121@gmail.com>
2019-03-07 01:56:09 +0000

    ignore neverallow fails

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,6 +23,9 @@
 
 DEVICE_PATH := device/xiaomi/whyred
 
+# To ignore nerverallows 
+SELINUX_IGNORE_NEVERALLOWS := true
+
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := sdm660
 TARGET_NO_BOOTLOADER := true
